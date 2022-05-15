d100b2177686b09533d590873bf13149f6ea33ec
Paula Andrea Gaitán <paulagaiitan@gmail.com>
2022-05-15 14:31:33 -0500

    Initial implementation of dependency injection with dagger hilt.

--- app/src/main/java/com/gaitan/dev/androidtestceiba/MainActivity.kt
+++ app/src/main/java/com/gaitan/dev/androidtestceiba/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.gaitan.dev.androidtestceiba.ui.theme.AndroidTestCeibaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/gaitan/dev/androidtestceiba/MyAndroidApp.kt
@@ -0,0 +1,8 @@
+package com.gaitan.dev.androidtestceiba
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class MyAndroidApp : Application()
